1fc613a791640bc0a9b9767ac7414fa756b2b46d
Aarash Feizi <aarash95@gmail.com>
2022-10-21 15:22:19 -0400

    add module to super

--- solo/data/imagefolder_missing_classes.py
+++ solo/data/imagefolder_missing_classes.py
@@ -141,7 +141,7 @@ class ImageFolderMissingClasses(ImageFolder):
         is_valid_file: Optional[Callable[[str], bool]] = None,
         class_to_idx = None,
     ):
-        super().__init__(root, transform=transform, target_transform=target_transform, loader=loader, is_valid_file=is_valid_file)
+        super(ImageFolderMissingClasses, self).__init__(root, transform=transform, target_transform=target_transform, loader=loader, is_valid_file=is_valid_file)
         
         extensions = IMG_EXTENSIONS if is_valid_file is None else None
         
