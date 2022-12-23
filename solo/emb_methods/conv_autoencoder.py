680090221fb98630e7d360c662c4fcdca6aa6667
Aarash Feizi <aarash95@gmail.com>
2022-12-23 14:30:03 -0500

    change maxunpool to upsample_bilinear

--- solo/emb_methods/conv_autoencoder.py
+++ solo/emb_methods/conv_autoencoder.py
@@ -16,16 +16,21 @@ class CAE(nn.Module):
         assert len(cfg.emb_model.sizes) == len(cfg.emb_model.kernel_sizes)
         self.is_train = False
         for idx in range(1, len(channel_sizes)):
-            layer = nn.Conv2d(in_channels=channel_sizes[idx - 1], out_channels=channel_sizes[idx], kernel_size=kernel_sizes[idx - 1])
+            layer = nn.Conv2d(in_channels=channel_sizes[idx - 1],
+                                out_channels=channel_sizes[idx],
+                                kernel_size=kernel_sizes[idx - 1])
             relu = nn.ReLU()
             maxpool = nn.MaxPool2d(kernel_size=2)
             encoder_layers.extend([layer, relu, maxpool])
 
         for idx in range(len(channel_sizes) - 1, 0, -1):
-            layer = nn.ConvTranspose2d(in_channels=channel_sizes[idx], out_channels=channel_sizes[idx - 1], kernel_size=kernel_sizes[idx - 1])
+            layer = nn.ConvTranspose2d(in_channels=channel_sizes[idx],
+                                        out_channels=channel_sizes[idx - 1],
+                                        kernel_size=kernel_sizes[idx - 1])
             relu = nn.ReLU()
-            maxunpool = nn.MaxUnpool2d(kernel_size=2)
-            dencoder_layers.extend([layer, relu, maxunpool])
+            # maxunpool = nn.MaxUnpool2d(kernel_size=2)
+            upsample = nn.Upsample(scale_factor=2, mode='bilinear')
+            dencoder_layers.extend([layer, relu, upsample])
         
 
         self.encoder = nn.Sequential(*encoder_layers)
