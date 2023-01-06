204a8140fb3f7e0e4bbfddd201953fe4bba48676
Aarash Feizi <aarash95@gmail.com>
2023-01-06 16:51:18 -0500

    add dtamoudle

---
+++ solo/data/base_datamodule.py
@@ -0,0 +1,50 @@
+import pytorch_lightning as pl
+import torch
+from solo.utils.misc import get_embeddings, get_sim_matrix
+from solo.data.nnclr2_dataset import NNCLR2_Dataset_Wrapper
+from solo.data.pretrain_dataloader import prepare_dataloader
+
+
+class BaseDataModule(pl.LightningDataModule):
+    def __init__(self, train_transforms=None, val_transforms=None, test_transforms=None, dims=None, model=None):
+        super().__init__(train_transforms, val_transforms, test_transforms, dims)
+        self.emb_dataloader = None
+        self.train_loader = None
+        self.val_loader = None
+        self.first_epoch = True
+        self.model = model
+
+    def set_emb_dataloder(self, loader):
+        self.emb_dataloader = loader
+    
+    def set_val_loader(self, loader):
+        self.val_loader = loader
+    
+    def set_train_loader(self, loader):
+        self.train_loader = loader
+
+    # overwriting
+    def val_dataloader(self):
+        return self.val_loader
+
+    # overwriting
+    def train_dataloader(self):
+        if self.first_epoch:
+            self.first_epoch = False
+            return self.train_loader
+        else:
+            print('Updating train_loader sim_matrix...')
+
+            assert self.emb_dataloader is not None
+
+            embeddings = get_embeddings(self.model, self.emb_train_loader)
+            _, emb_sim_matrix = get_sim_matrix(embeddings, gpu=torch.cuda.is_available())
+            train_dataset = NNCLR2_Dataset_Wrapper(dataset=self.train_loader.dataset.dataset,
+                                                    sim_matrix=emb_sim_matrix,
+                                                    num_nns=self.train_loader.dataset.num_nns,
+                                                    num_nns_choice=self.train_loader.dataset.num_nns_choice)
+
+            self.train_loader = prepare_dataloader(
+                train_dataset, batch_size=self.train_loader.batch_size, num_workers=self.train_loader.num_workers)
+            
+            return self.train_loader
