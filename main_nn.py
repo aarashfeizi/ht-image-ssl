795fc6b3cc196af542572f4d44dfe0fa237e45f3
Aarash Feizi <aarash95@gmail.com>
2023-03-22 13:53:02 -0400

    fig bug

--- main_nn.py
+++ main_nn.py
@@ -319,7 +319,7 @@ def main(cfg: DictConfig):
             if cfg.data.clustering_algo == 'louvainW':
                 clust_lbls, knn_graph = misc.get_louvain_clusters_weighted(emb_sim_matrix, dist_matrix=emb_dist_matrix, seed=cfg.seed, threshold=cfg.data.nn_threshold)
             elif cfg.data.clustering_algo == 'louvainU':
-                clust_lbls, knn_graph = misc.get_louvain_clusters_unweighted(emb_sim_matrix, dist_matrix=emb_dist_matrix, seed=cfg.seed, k=cfg.data.num_nns_choice)
+                clust_lbls, knn_graph = misc.get_louvain_clusters_unweighted(emb_sim_matrix, dist_matrix=emb_dist_matrix, seed=cfg.seed, k=cfg.data.num_nns_choice + 1)
         
         assert len(train_dataset) == len(embeddings)
 
