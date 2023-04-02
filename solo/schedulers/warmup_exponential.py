0c2cfbdb756aff88df74502721c5905a93bd995a
Aarash Feizi <aarash95@gmail.com>
2023-04-02 10:46:31 -0400

    fix bug

--- solo/schedulers/warmup_exponential.py
+++ solo/schedulers/warmup_exponential.py
@@ -1,13 +1,12 @@
 class WarmUpExponentialLR():
   def __init__(self, gamma, warmup_epochs):
     self.gamma = gamma
+    self.coef = 1.0
     self.warmup_epochs = warmup_epochs
   
   def __call__(self, epoch):
     if (epoch + 1) <= self.warmup_epochs:
       return (epoch + 1) / self.warmup_epochs 
     else:
-      lr_coeff = self.gamma
-      self.gamma *= self.gamma
-      return lr_coeff
-    
+      self.coef *= self.gamma
+      return self.coef
