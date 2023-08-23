bbc01a339bf153384beeddf905eb3f32affa10e4
Aarash Feizi <aarash95@gmail.com>
2023-08-23 17:15:35 -0400

    fix medmnist issue

--- solo/data/medmnist.py
+++ solo/data/medmnist.py
@@ -4,12 +4,12 @@ import pdb
 
 class PathMNIST(PathMNIST_T):
     def __getitem__(self, index):
-        batch = super().__getitem__(index)
-        pdb.set_trace()
-        return batch
+        img, target = super().__getitem__(index)
+        target = target[0]
+        return img, target 
 
 class TissueMNIST(TissueMNIST_T):
     def __getitem__(self, index):
-        batch = super().__getitem__(index)
-        pdb.set_trace()
-        return batch
+        img, target = super().__getitem__(index)
+        target = target[0]
+        return img, target
