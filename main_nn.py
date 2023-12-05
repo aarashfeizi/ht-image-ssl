0df7a66175b0b644977eb0e77bb4d6e2616e0275
Aarash Feizi <aarash95@gmail.com>
2023-12-05 12:33:04 -0500

    fix bug

--- main_nn.py
+++ main_nn.py
@@ -636,7 +636,7 @@ def main(cfg: DictConfig):
             "reload_dataloaders_every_n_epochs": cfg.data.reload_freq,
             "log_every_n_steps": 500,
             #"progress_bar_refresh_rate": 0, # turn off progress bar
-            "strategy": DDPStrategy(find_unused_parameters=True) if cfg.strategy == "ddp" else cfg.strategy,
+            "strategy": DDPStrategy(find_unused_parameters=(cfg.method != 'mae')) if cfg.strategy == "ddp" else cfg.strategy,
         }
     )
     trainer = Trainer(**trainer_kwargs)
